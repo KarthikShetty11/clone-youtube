3f74c865f17a3920857e05540b934f976b2fd2b4
KarthikShetty11 <kvshetty27@gmail.com>
2022-03-06 01:27:36 +0530

    filters

--- app.js
+++ app.js
@@ -51,10 +51,17 @@ const makeVideoCard = (data) => {
 
 const searchInput = document.querySelector('.search-bar');
 const searchBtn = document.querySelector('.search-btn');
+
+const filteroptions = document.querySelector('.filter-options');
+
 let searchLink = "https://www.youtube.com/results?search_query=";
 
 searchBtn.addEventListener('click', () => {
     if(searchInput.value.length){
         location.href = searchLink + searchInput.value;
     }
+})
+
+filteroptions.addEventListener('click', () => {
+location.href = searchLink + filteroptions.value;
 })
